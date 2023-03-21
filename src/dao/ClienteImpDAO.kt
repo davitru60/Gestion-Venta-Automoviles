d5c1afec950c70f547cb80a3c4c3f3349082787a
pinoxx64 <loscanos3raul@gmail.com>
2023-03-21 19:29:43 +0100

    Val sql

--- src/dao/ClienteImpDAO.kt
+++ src/dao/ClienteImpDAO.kt
@@ -2,15 +2,15 @@ package dao
 
 class ClienteImpDAO : IClienteDAO{
     override fun añadirCliente(){
-
+        val sql= "insert into clientes values ('','','','');"
     }
     override fun eliminarCliente(){
-
+        val sql= "insert into clientes values ('','','','');"
     }
     override fun verListaCliente(){
-
+        val sql= "select * from clientes;"
     }
     override fun BuscarCliente(){
-
+        val sql= "select * from clientes where nombre='';"
     }
 }
