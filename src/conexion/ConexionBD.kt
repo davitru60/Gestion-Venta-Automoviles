df4626cd05c6888f88a7365cbc043005207b5ef4
davitru60 <davitru60@gmail.com>
2023-03-21 19:59:21 +0100

    commit v1.0

--- src/conexion/ConexionBD.kt
+++ src/conexion/ConexionBD.kt
@@ -1,4 +1,38 @@
 package conexion
 
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.PreparedStatement
+import java.sql.SQLException
+
 class ConexionBD {
+    private val url = "jdbc:mysql://localhost/automoviles"
+    private val driver = "com.mysql.cj.jdbc.Driver"
+    private val usuario = "root"
+    private val contrasenia = ""
+    private var conex: Connection? = null
+
+    fun conectar() {
+        try{
+            Class.forName(driver)
+            conex= DriverManager.getConnection(url,usuario,contrasenia)
+        }catch(e: SQLException){
+            e.printStackTrace()
+        }catch (e:ClassNotFoundException){
+            e.printStackTrace()
+        }
+
+    }
+
+    fun desconectar(){
+        try{
+            conex?.close()
+        }catch(e: SQLException){
+            println("Algo fue mal cerrando la base de datos")
+        }
+    }
+
+    fun getPreparedStatement(sql: String): PreparedStatement? {
+        return conex?.prepareStatement(sql)
+    }
 }
