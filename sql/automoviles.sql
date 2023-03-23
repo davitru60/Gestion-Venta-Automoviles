2d5efc047043aab7cd7338313de3a6dfea31c52e
lmexe18 <laurampego@gmail.com>
2023-03-23 13:55:33 +0100

    corrección

--- sql/automoviles.sql
+++ sql/automoviles.sql
@@ -19,7 +19,7 @@ CREATE TABLE ventas (
     id INT PRIMARY KEY AUTO_INCREMENT,
     automovil_id INT NOT NULL,
     cliente_id INT NOT NULL,
-    fecha VARCHAR NOT NULL,
+    fecha VARCHAR(10) NOT NULL,
     precio_venta DECIMAL(10,2) NOT NULL,
     FOREIGN KEY (automovil_id) REFERENCES automoviles(id) ON DELETE CASCADE,
     FOREIGN KEY (cliente_id) REFERENCES clientes(id) ON DELETE CASCADE
