ed06aa1c8643b852aec263c68ba6c138a68e2c6c
davitru60 <davitru60@gmail.com>
2023-03-24 11:05:25 +0100

    cambio

--- src/dao/IClienteDAO.kt
+++ src/dao/IClienteDAO.kt
@@ -1,10 +1,4 @@
 package dao
 
-import no_dao.Cliente
-
 interface IClienteDAO {
-    fun aniadirCliente(cliente:Cliente)
-    fun eliminarCliente(id:Int)
-    fun verListaCliente():List<Cliente>
-    fun buscarCliente(id: Int):Cliente?
 }
