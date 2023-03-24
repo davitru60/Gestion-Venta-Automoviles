03996aa61132b3ba760618c0bec6c81db1ba7c4f
davitru60 <davitru60@gmail.com>
2023-03-24 12:54:32 +0100

    Update main.kt

--- src/main.kt
+++ src/main.kt
@@ -3,6 +3,7 @@ import dao.AutomovilImpDAO
 import dao.ClienteImpDAO
 import dao.VentaImpDAO
 import no_dao.Automovil
+import no_dao.AutomovilFuncionalidades
 import no_dao.Cliente
 import java.lang.NumberFormatException
 
@@ -20,32 +21,6 @@ fun main(){
 
 }
 val ventaDAO = VentaImpDAO()
-
-
-fun menuVentas(){
-    var s:Int=0
-    var venta=MenuVentas()
-    println()
-    println("¿Qué opción de venta desea?")
-    println("1. Insertar ventas")
-    println("2. Obtener los datos de alguna venta.")
-    println("3. Obtener los datos de todas las ventas")
-    println("4. Actualizar una venta.")
-    println("5. Borrar una venta")
-    try {
-        s = readln().toInt()
-    } catch (e:Exception){
-        println("ERROR: La opción introducida es incorrecta")
-    }
-    when (s){
-        1->venta.insertarListaVentas()
-        2->venta.obtenerVentaMedianteID()
-        3->venta.obtenerTodasLasVentas()
-        4->venta.actualizarVentas()
-        5->venta.borrarVenta()
-    }
-}
-
 
 fun menuGeneral(automovilDAO: AutomovilImpDAO){
     var seleccion: Int
@@ -77,3 +52,60 @@ fun menuGeneral(automovilDAO: AutomovilImpDAO){
     } while (seleccion != 0)
 }
 
+fun menuAutomoviles(automovilDAO: AutomovilImpDAO) {
+    val automovilFuncionalidades = AutomovilFuncionalidades()
+    var seleccion: Int
+    var str: String
+    do {
+        println("\n")
+        println("¿Qué deseas hacer?")
+        println("1. Insertar automovil")
+        println("2. Obtener automoviles por rango de precios")
+        println("3. Obtener automovil por marca")
+        println("4. Obtener todos los automoviles")
+        println("5. Actualizar precio de un automovil")
+        println("6. Eliminar automovil")
+
+        str = readln()
+        seleccion =
+            try {
+                str.toInt()
+            } catch (e: Exception) {
+                -1
+            }
+        when (seleccion) {
+            1 -> automovilFuncionalidades.insertarAutomovil(automovilDAO)
+            2 -> automovilFuncionalidades.actualizarPrecioAutomovil(automovilDAO)
+            3 -> automovilFuncionalidades.obtenerAutomovilesPorRangoDePrecio(automovilDAO)
+            4 -> automovilFuncionalidades.obtenerAutomovilesPorMarca(automovilDAO)
+            5 -> automovilFuncionalidades.obtenerTodosLosAutomoviles(automovilDAO)
+            6 -> automovilFuncionalidades.eliminarAutomovil(automovilDAO)
+            else -> println("Opcion incorrecta")
+        }
+    } while (seleccion != 0)
+
+}
+
+fun menuVentas(){
+    var s:Int=0
+    var venta=MenuVentas()
+    println()
+    println("¿Qué opción de venta desea?")
+    println("1. Insertar ventas")
+    println("2. Obtener los datos de alguna venta.")
+    println("3. Obtener los datos de todas las ventas")
+    println("4. Actualizar una venta.")
+    println("5. Borrar una venta")
+    try {
+        s = readln().toInt()
+    } catch (e:Exception){
+        println("ERROR: La opción introducida es incorrecta")
+    }
+    when (s){
+        1->venta.insertarListaVentas()
+        2->venta.obtenerVentaMedianteID()
+        3->venta.obtenerTodasLasVentas()
+        4->venta.actualizarVentas()
+        5->venta.borrarVenta()
+    }
+}
